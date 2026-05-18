ac756febc58aab381012d1984bfe5d445d2f775e
(Yuto0508) <(yuto4139@outlook.jp)>
2026-05-18 09:17:00 +0900

    feat: add home route and HomeController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>vlogトップページ</title>
+</head>
+
+<body>
+	<h1>ゆーとのvlog</h1>
+	<p>ようこそ！</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+// ルーティングの読み込み
 use Illuminate\Support\Facades\Route;
+// HomeControllerの読み込み
+use App\Http\Controllers\HomeController;
 
+// トップページ（/）にアクセスしたらwelcomeビューを表示する
 Route::get('/', function () {
     return view('welcome');
 });
+
+// /homeにアクセスしたらHomeControllerのindexメソッドを実行する
+Route::get('/home', [HomeController::class, 'index']);
